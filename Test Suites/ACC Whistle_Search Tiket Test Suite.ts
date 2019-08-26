0cb21208bd06dd5bc86e3df3bfd3f5fcf547a6ee
ASUS <ASUS@LAPTOP-KVQOJMPC>
2019-08-26 10:15:40 +0700

    adding data excel

--- Test Suites/ACC Whistle_Search Tiket Test Suite.ts
+++ Test Suites/ACC Whistle_Search Tiket Test Suite.ts
@@ -17,15 +17,15 @@
       <testCaseId>Test Cases/ACC Whistle_Cari tiket</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>4c6eb011-5f89-4e80-98cb-78896b652dbe</id>
+         <id>254ba83c-82e6-49d0-ba7f-a40871c1bac0</id>
          <iterationEntity>
-            <iterationType>SPECIFIC</iterationType>
-            <value>1</value>
+            <iterationType>RANGE</iterationType>
+            <value>1-3</value>
          </iterationEntity>
          <testDataId>Data Files/no_tiket</testDataId>
       </testDataLink>
       <variableLink>
-         <testDataLinkId>4c6eb011-5f89-4e80-98cb-78896b652dbe</testDataLinkId>
+         <testDataLinkId>254ba83c-82e6-49d0-ba7f-a40871c1bac0</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>no_tiket</value>
          <variableId>ffcaec23-c669-4758-8b81-231781d91b60</variableId>
